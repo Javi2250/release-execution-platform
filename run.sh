2e40c44d513620b9016b49e2bdc2438d8b17839e
Javi2250 <javier.perez.celada@gmail.com>
2026-02-10 00:42:35 +0100

    Add ru script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+echo "🚀 Ejecutando run.sh"
+echo "📍 Directorio actual: $(pwd)"
+echo "👤 Usuario: $(whoami)"
+echo "🐧 Kernel: $(uname -a)"
+
+echo "✅ Script ejecutado correctamente"
+
